36d58c33c164660bc08edb0bd5e3ff2667b43b44
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-10-28 23:47:15 -0300

    cSharp + OOP

---
+++ 001-LoopFor/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Executando projeto 001 - Investimento a Longo Prazo");
+
+double rendimentoAoMes = 1.005;
+double investimento = 0;
+double investimentoMensal = 1000;
+int anos = 30;
+for (int ano = 1; ano <= anos; ano++)
+{
+    for(int mes = 1; mes <= 12; mes++)
+    {
+        investimento *= rendimentoAoMes;
+        investimento += investimentoMensal;
+    }
+    rendimentoAoMes += 0.001;
+}
+
+Console.WriteLine($"Depois de {anos} anos, você terá um total de: " + investimento);
---
+++ 002-EncadeamentoFor/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Executando projeto 002 - Encadeando For");
+// Saída do seguinte desenho:
+//*
+//**
+//***
+//****
+//*****
+
+for(int linha = 1; linha < 10; linha++)
+{
+    for(int colunas = 1; colunas < linha; colunas++)
+    {
+        Console.Write('*');
+    }
+    Console.WriteLine();
+}
+
+// Printar numeros multiplos de 3
+int coeficiente = 4;
+int inicio = 4;
+int total = 104;
+int numero = 0;
+
+Console.WriteLine($"Múltiplos de: {coeficiente}. Iniciando em {inicio} até {total}");
+
+for(numero = 1; numero <= total; numero++)
+{
+    if(numero % coeficiente == 0)
+    {
+        Console.WriteLine(numero);
+    }
+}
+Console.WriteLine();
+// Outra abordagem para printar números multiplos de 3
+for(int i = 3; i < 100; i += 3)
+{
+    Console.WriteLine(i);
+}
+
+// Exercício
+Console.WriteLine("Imprimir fatorial");
+
+Console.WriteLine(Fatorial(6));
+
+int Fatorial(int num)
+{
+    int sum = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
