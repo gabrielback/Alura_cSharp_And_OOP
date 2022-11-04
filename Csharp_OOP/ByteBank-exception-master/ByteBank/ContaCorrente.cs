161e271a9145a665a5a9114cc1ff77d324efe5e3
gabrielback12 <gabrielluiz-back@hotmail.com>
2022-11-04 00:37:32 -0300

    ByteBank-exceptioons

--- Csharp_OOP/ByteBank-exception-master/ByteBank/ContaCorrente.cs
+++ Csharp_OOP/ByteBank-exception-master/ByteBank/ContaCorrente.cs
@@ -5,6 +5,7 @@ namespace ByteBank
     public class ContaCorrente
     {
         public Cliente Titular { get; set; }
+        public static double TaxaOperacao { get; private set; }
 
         public static int TotalDeContasCriadas { get; private set; }
 
@@ -52,6 +53,9 @@ namespace ByteBank
         {
             Agencia = agencia;
             Numero = numero;
+
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
 
             TotalDeContasCriadas++;
         }
--- Csharp_OOP/ByteBank-exception-master/ByteBank/Program.cs
+++ Csharp_OOP/ByteBank-exception-master/ByteBank/Program.cs
@@ -10,7 +10,60 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
+
+            #region
+            /*ContaCorrente conta1 = new ContaCorrente(7480, 874250);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            ContaCorrente conta2 = new ContaCorrente(7480, 874250);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            ContaCorrente conta3 = new ContaCorrente(7480, 874250);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.ReadLine();*/
+            #endregion
+
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Ocorreu um erro! Não é possível dividir um numero por 0! Mensagem: " + e.Message);
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Aconteceu um erro!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Aconteceu um erro! " + e.Message);
+            }
+            Console.ReadKey();
+        }
+        public static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            Console.WriteLine("O resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch
+            {
+                Console.WriteLine("Exceção com número = " + numero + " e divisor = " + divisor);
+                throw;
+            }
         }
     }
 }
